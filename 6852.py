88e7730a73db9a70393bd520123beab21e99f43f
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2023-03-20 20:18:48 +0900

    Update 6852.py
    edit version

--- 6852.py
+++ 6852.py
@@ -6,18 +6,15 @@ def Input_Data():
     return R
 
 
-ans = 0
+ans = -1
 # 입력 받은 부분
 R = Input_Data()
 
 # 여기서부터 작성
-if R > (2*((R-1)**2))**(1/2):
-    ans = ((R-1)**2)*4
+if 2*((R-1)**2) < R**2 :
+    ans = 4*((R-1)**2)
 else:
-    ans = ( ((R-2)**2) + (R-2)*2 )*4
-
-
-
+    ans = 4*((R-1)**2-1)
 
 # 출력하는 부분
 print(ans)
